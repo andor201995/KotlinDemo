6eb4c1ee53ede56edda02ac23ec6b697cf6b6341
Anmol Srivastava <anmol.s@zohocorp.com>
2017-09-21 19:22:45 +0530

    Initial Commit and hello to Kotlin

---
+++ DemoKotlin/src/HelloKotlin.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {  //entry point bye bye JAVA big Main function signature (:p)
+    println("Hello from Kotlin")
+}
---
+++ DemoKotlin/src/HelloVariable.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var fName: String = "Anmol"
+    var lName = "Srivastava"
+    val id = 1031310671
+    var dept: String? //? to not check null
+    dept = "Computer Science"
+}
